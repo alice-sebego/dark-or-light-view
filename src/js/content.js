7abf0c5afebaf24a9742f852080fb6b0f95ad435
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-07-08 23:19:25 +0200

    Store contents on its own file

---
+++ src/js/content.js
@@ -0,0 +1,49 @@
+const posts = {
+    
+    contrat : {
+        title : "Un contrat de professionnalisation",
+        pessimitic : "Ça coûtera cher à la boîte",
+        dark_img:"dist/assets/contrat-professionnalisation-alternance-investissement.jpg",
+        alt_dark:"Un contrat de professionnalisation est un investissement",
+        optimistic:"Après l'investissement vient le retour sur investissement",
+        light_img:"dist/assets/contrat-professionnalisation-retour-sur-investissement.jpg",
+        alt_light:"Un alternant peut être un bon retour sur investissement"
+    },
+
+    age : {
+        title : "41 ans !?!",
+        pessimitic : "Une galère qui s'intégrera pas à l'équipe donc non merci",
+        dark_img:"dist/assets/diversite-age-dans-une-equipe-travail-dark.jpg",
+        alt_dark:"Un élément de 41 ans dans une équipe de travail ?",
+        optimistic:"Une équipe est un bon jeu de patience où chaque fragment de la vie de l'équipe, une fois rassemblé, compose votre puzzle.",
+        light_img:"dist/assets/diversite-age-dans-une-equipe-travail-light.jpg",
+        alt_light:"La diversité dans une équipe nous vaccine des biais cognitifs ;)"
+    },
+
+    junior : {
+        title : "Les juniors, sérieux !?!",
+        pessimitic : "Sans façon ! Trop de freins : Entre le manque d'expérience et d'autonomie ...",
+        dark_img:"https://tenor.com/bAjWx.gif",
+        alt_dark:"Need experience for job, need job for experience",
+        optimistic:"\"L'expérience de chacun est le trésor de tous.\" - Gérard de Nerval",
+        light_img:"https://tenor.com/0E2L.gif",
+        alt_light:"La diversité dans une équipe nous vaccine des biais cognitifs ;)"
+    },
+
+    reconverti : {
+        title : "Oula ! Une reconvertie",
+        pessimitic : "Oula ! La plupart d'eux est entré dans le métier juste parce qu'ils ont vu de la lumière",
+        dark_img:"dist/assets/developpeur-reconverti-avantage-ou-inconvenient-dark.jpg",
+        alt_dark:"Une reconvertion professionnelle, fruit du hasard",
+        optimistic:"L'étrangeté n'est qu'une affaire de point de vue ou de référentiel Jean-Michel Calvez <br>Vous avez cliqué pour basculer en mode light, non ? Pourtant, je ne vous trouve ni étrange, ni bizarre",
+        light_img:"dist/assets/developpeur-reconverti-avantage-ou-inconvenient-light.jpg",
+        alt_light:"Une reconvertion professionnelle, fruit d'un choix de métier"
+    }
+}
+
+const mainText = {
+    dark : "Noir c'est noir, <br>Il n'y a plus d'espoir <br>... ou pas !",
+    light: "On ne peut pas peindre du blanc sur du blanc, du noir sur du noir.<br>Nous sommes tous des révélateurs des uns et des autres.<br>Manu Dibango"
+}
+
+export { posts, mainText }
