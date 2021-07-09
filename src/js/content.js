15b4b8d9c5ff91162b0d8297987782f3dc2273c6
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-07-09 03:06:15 +0200

    Handle initialization and switch of posts display

--- src/js/content.js
+++ src/js/content.js
@@ -2,7 +2,7 @@ const posts = {
     
     contrat : {
         title : "Un contrat de professionnalisation",
-        pessimitic : "Ça coûtera cher à la boîte",
+        pessimistic : "Ça coûtera cher à la boîte",
         dark_img:"dist/assets/contrat-professionnalisation-alternance-investissement.jpg",
         alt_dark:"Un contrat de professionnalisation est un investissement",
         optimistic:"Après l'investissement vient le retour sur investissement",
@@ -12,7 +12,7 @@ const posts = {
 
     age : {
         title : "41 ans !?!",
-        pessimitic : "Une galère qui s'intégrera pas à l'équipe donc non merci",
+        pessimistic : "Une galère qui s'intégrera pas à l'équipe donc non merci",
         dark_img:"dist/assets/diversite-age-dans-une-equipe-travail-dark.jpg",
         alt_dark:"Un élément de 41 ans dans une équipe de travail ?",
         optimistic:"Une équipe est un bon jeu de patience où chaque fragment de la vie de l'équipe, une fois rassemblé, compose votre puzzle.",
@@ -22,7 +22,7 @@ const posts = {
 
     junior : {
         title : "Les juniors, sérieux !?!",
-        pessimitic : "Sans façon ! Trop de freins : Entre le manque d'expérience et d'autonomie ...",
+        pessimistic : "Sans façon ! Trop de freins : Entre le manque d'expérience et d'autonomie ...",
         dark_img:"<div class='tenor-gif-embed' data-postid='21010459' data-share-method='host' data-width='100%' data-aspect-ratio='1.2607594936708861'><a href='https://tenor.com/view/experience-need-experience-need-job-need-experience-for-job-need-job-for-experience-gif-21010459'>Experience Need Experience GIF</a> from <a href='https://tenor.com/search/experience-gifs'>Experience GIFs</a></div><script type='text/javascript' async src='https://tenor.com/embed.js'></script>",
         alt_dark:"Need experience for job, need job for experience",
         optimistic:"\"L'expérience de chacun est le trésor de tous.\" - Gérard de Nerval",
@@ -32,7 +32,7 @@ const posts = {
 
     reconverti : {
         title : "Oula ! Une reconvertie",
-        pessimitic : "Oula ! La plupart d'eux est entré dans le métier juste parce qu'ils ont vu de la lumière",
+        pessimistic : "Oula ! La plupart d'eux est entré dans le métier juste parce qu'ils ont vu de la lumière",
         dark_img:"dist/assets/developpeur-reconverti-avantage-ou-inconvenient-dark.jpg",
         alt_dark:"Une reconvertion professionnelle, fruit du hasard",
         optimistic:"L'étrangeté n'est qu'une affaire de point de vue ou de référentiel Jean-Michel Calvez <br>Vous avez cliqué pour basculer en mode light, non ? Pourtant, je ne vous trouve ni étrange, ni bizarre",
--- src/js/main.js
+++ src/js/main.js
@@ -3,30 +3,99 @@ import { posts, mainText } from "./content.js";
 const $section = document.querySelector("section");
 const $switch = document.querySelector("#switch");
 
-for(let [key, val] of Object.entries(posts)){
+
+// INIT
+const init = () => {
     
-    let {title, pessimitic, dark_img, alt_dark, optimistic, light_img, alt_light} = val;
+    for(let [key, val] of Object.entries(posts)){
+        
+        let {title, pessimistic, dark_img, alt_dark} = val;
+ 
+        if(key !== "junior"){
+            const viewPoint = document.createElement("div");
+            viewPoint.setAttribute("class", "posts");
+            viewPoint.innerHTML = ` 
+            <h2>${title}</h2>
+            <p>${pessimistic}</p>
+            <picture>
+                <img src="${dark_img}" alt="${alt_dark}">
+            </picture>
+            `;
+            $section.appendChild(viewPoint);
+        } else {
+            const viewPoint = document.createElement("div");
+            viewPoint.setAttribute("class", "posts");
+            viewPoint.innerHTML = ` 
+            <h2>${title}</h2>
+            <p>${pessimistic}</p>
+            ${dark_img}
+            `;
+            $section.appendChild(viewPoint);
+        }  
     
-    if(key !== "junior"){
-        const viewPoint = document.createElement("div");
-        viewPoint.setAttribute("class", "posts");
-        viewPoint.innerHTML = ` 
-        <h2>${title}</h2>
-        <p>${pessimitic}</p>
-        <picture>
-            <img src="${dark_img}" alt="${alt_dark}">
-        </picture>
-        `;
-        $section.appendChild(viewPoint);
-    } else {
-        const viewPoint = document.createElement("div");
-        viewPoint.setAttribute("class", "posts");
-        viewPoint.innerHTML = ` 
-        <h2>${title}</h2>
-        <p>${pessimitic}</p>
-        ${dark_img}
-        `;
-        $section.appendChild(viewPoint);
-    }
-   
+    }     
+    
 }
+
+init();
+
+const switchPosts = () => {
+    
+    $section.innerHTML = "";
+
+    for(let [key, val] of Object.entries(posts)){
+        
+        let {title, pessimistic, dark_img, alt_dark, optimistic, light_img, alt_light} = val;
+     
+        if(!$switch.checked){
+    
+            if(key !== "junior"){
+                const viewPoint = document.createElement("div");
+                viewPoint.setAttribute("class", "posts");
+                viewPoint.innerHTML = ` 
+                <h2>${title}</h2>
+                <p>${pessimistic}</p>
+                <picture>
+                    <img src="${dark_img}" alt="${alt_dark}">
+                </picture>
+                `;
+                $section.appendChild(viewPoint);
+            } else {
+                const viewPoint = document.createElement("div");
+                viewPoint.setAttribute("class", "posts");
+                viewPoint.innerHTML = ` 
+                <h2>${title}</h2>
+                <p>${pessimistic}</p>
+                ${dark_img}
+                `;
+                $section.appendChild(viewPoint);
+            }
+        } else {
+            if(key !== "junior"){
+                const viewPoint = document.createElement("div");
+                viewPoint.setAttribute("class", "posts");
+                viewPoint.innerHTML = ` 
+                <h2>${title}</h2>
+                <p>${optimistic}</p>
+                <picture>
+                    <img src="${light_img}" alt="${alt_light}">
+                </picture>
+                `;
+                $section.appendChild(viewPoint);
+            } else {
+                const viewPoint = document.createElement("div");
+                viewPoint.setAttribute("class", "posts");
+                viewPoint.innerHTML = ` 
+                <h2>${title}</h2>
+                <p>${optimistic}</p>
+                ${light_img}
+                `;
+                $section.appendChild(viewPoint);
+            }
+    
+        }
+       
+    }
+}
+
+$switch.addEventListener("change", switchPosts);
