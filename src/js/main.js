4f03ab652e33fbfe182c6503f271d08415804615
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-07-09 20:52:39 +0200

    Handle a scroll animation for event on swith input

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,15 @@ import HandlePost from "./handle-post.js";
 
 const $section = document.querySelector("section");
 const $switch = document.querySelector("#switch");
+
 // Lauch handle of post
 const app = new HandlePost($section, $switch, posts, mainText);
 app.init();
-$switch.addEventListener("change", app.switchPosts);
+
+$switch.addEventListener("change", () => {
+    app.switchPosts();
+    window.scrollTo(0, 0);
+});
 
 // Lauch AOS.js
 AOS.init();
