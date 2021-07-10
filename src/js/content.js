98e669acbc692ff2a59266a63526763d20204005
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-07-10 23:51:40 +0200

    Update contents of posts

--- src/js/content.js
+++ src/js/content.js
@@ -11,10 +11,10 @@ const posts = {
     },
 
     age : {
-        title : "41 ans !?",
-        pessimistic : "Une galère qui s'intégrera pas à l'équipe donc non merci",
+        title : "35, 36, ...41 ans !?",
+        pessimistic : "Une galère pour l'intégrer à l'équipe donc non merci",
         dark_img:"dist/assets/diversite-age-dans-une-equipe-travail-dark.jpg",
-        alt_dark:"Un élément de 41 ans dans une équipe de travail ?",
+        alt_dark:"Un nouvel élément de 41 ans dans une équipe de travail ?",
         optimistic:"Une équipe est un bon jeu de patience où chaque fragment de la vie de l'équipe, une fois rassemblé, compose votre puzzle.",
         light_img:"dist/assets/diversite-age-dans-une-equipe-travail-light.jpg",
         alt_light:"La diversité dans une équipe nous vaccine des biais cognitifs ;)"
@@ -31,11 +31,11 @@ const posts = {
     },
 
     reconverti : {
-        title : "Oula ! Une reconvertie",
+        title : "Oula ! Un.e reconverti.e",
         pessimistic : "La plupart d'eux est entrée dans le métier juste parce qu'ils ont vu de la lumière",
         dark_img:"dist/assets/developpeur-reconverti-avantage-ou-inconvenient-dark.jpg",
         alt_dark:"Une reconvertion professionnelle, fruit du hasard",
-        optimistic:"L'étrangeté n'est qu'une affaire de point de vue ou de référentiel - Jean-Michel Calvez <br>Vous avez cliqué pour basculer en mode light, non ? Pourtant, je ne vous trouve ni étrange, ni bizarre",
+        optimistic:"L'étrangeté n'est qu'une affaire de point de vue ou de référentiel - Jean-Michel Calvez <br>Vous avez cliqué pour basculer en mode light, non ? Êtez-vous étrange pour autant ?",
         light_img:"dist/assets/developpeur-reconverti-avantage-ou-inconvenient-light.jpg",
         alt_light:"Une reconvertion professionnelle, fruit d'un choix de métier"
     }
