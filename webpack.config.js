d7db43129bf80aaafe0752abc7ef2bfe7749222a
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-07-13 18:36:55 +0200

    Add Babel on this project

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,21 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
